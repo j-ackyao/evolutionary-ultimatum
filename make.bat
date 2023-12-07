9ba5c68befcfcd133805614bf5da0fc6d5d7e254
jackyao <jackyao2004@gmail.com>
2023-12-07 15:36:19 -0800

    attempt to compile on pi

--- make.bat
+++
@@ -1,1 +0,0 @@
-gcc -o main *.c *.h
--- player.c
+++ player.c
@@ -7,7 +7,7 @@
 
 struct player init_player() {
 	double a = get_rand_range(0, 10);
-	double b = get_rand(0, 10);
-	struct player p = {get_rand(0, 10), min(a, b), max(a, b)};
+	double b = get_rand_range(0, 10);
+	struct player p = {get_rand_range(0, 10), min(a, b), max(a, b)};
 	return p;
 }
